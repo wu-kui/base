edd54c6fcd6b522704f99f5f4c013c73c83a958c
wukui <824363345@qq.com>
2017-10-26 21:58:25 +0800

    2017-10-26 可以得到带宽信息了

--- change_network.py
+++ change_network.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import urllib2, sys, time
+import urllib2, sys, time, json
 
-username='wukui@huitongjy.com'
-password='MjAxN+W5tDEw5pyIMjT'
+username='user'
+password='password'
 get_token_api='http://api2.capitalonline.net/gic/v1/get_token/'
 log_file = './change_network.log'
 
@@ -15,7 +15,7 @@ log_file = './change_network.log'
 def now():
     # 时间格式
     ISOTIMEFORMAT = '%Y-%m-%d %X'
-    return time.strftime( ISOTIMEFORMAT, time.gmtime( time.time() ))
+    return time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ))
 
 
 
@@ -39,21 +39,74 @@ def return_token(url, username, password):
         with open(log_file, 'a') as f:
             datetime = now()
             logs = '[ ' + datetime + ' ]' + ' ' + '获取ToKen失败' + ' ' + str(status) + '\n'
+            f.write(logs)
 
 
 
 
-#    获取虚拟数据中心的带宽信息
-def get_datacenter_info(data_center_id, token):
-    api = 'http://api2.capitalonline.net/gic/v1/bandwidth/public/'
-    url = api + data_center_id
+#    获取虚拟数据中心的带宽，直接返回带宽，单位是M
+def get_datacenter_info(token, vcenter_appid):
+    api = 'http://api2.capitalonline.net/gic/v1/app/info/'
+    url = api + '?app_id=' + vcenter_appid
 
     http = urllib2.Request(url)
     http.add_header('token', token)
     r = urllib2.urlopen(http)
-
     info = r.read()
-    print(info)
+    info = eval(info)
+
+    if info["status"] == "success":
+        alldata = dict(info["data"][0])
+        alldata = alldata['net']
+        for net in alldata:
+            if net["type"] and net["type"] == "public":
+                qos = net["qos"]
+                return qos
+            else:
+                with open(log_file, 'a') as f:
+                    datetime = now()
+                    logs = '[ ' + datetime + ' ]' + ' ' + '没有找到带宽信息 (qos)' + '\n'
+                    f.write(logs)
+                sys.exit(0)
+    else:
+        with open(log_file, 'a') as f:
+            datetime = now()
+            logs = '[ ' + datetime + ' ]' + ' ' + '获取带宽失败:[ ' + url + ' ]' + '\n'
+            f.write(logs)
+
+
+
+
+#   获取虚拟数据中心的app_id
+def get_vcenter_appid(token,vcenter_name):
+    api = 'http://api2.capitalonline.net/gic/v1/app/list/'
+    url = api
+
+    http = urllib2.Request(url)
+    http.add_header('token', token)
+    r = urllib2.urlopen(http)
+    vcenter_info = r.read()
+    vcenter_info = eval(vcenter_info)
+
+    if vcenter_info["status"] == "success":
+        for vcenter in vcenter_info["data"]:
+            if vcenter["name"] == vcenter_name:
+                vcenter_appid = vcenter["app_id"]
+                break
+            else:
+                continue
+    else:
+        with open(log_file, 'a') as f:
+            datetime = now()
+            logs = '[ ' + datetime + ' ]' + ' ' + '	列虚拟数据中心失败' + ' ' + '\n'
+            f.write(logs)
+    if not vcenter_appid:
+        with open(log_file, 'a') as f:
+            datetime = now()
+            logs = '[ ' + datetime + ' ]' + ' ' + '	没有找到该数据中心:[ ' + str(vcenter_name) + ' ]' + '\n'
+            f.write(logs)
+        sys.exit(1)
+    return  vcenter_appid
 
 
 
@@ -61,6 +114,15 @@ def get_datacenter_info(data_center_id, token):
 
 if __name__ == "__main__":
 
+    # 用于接口返回的json字串中，关键字的大小写与python不一致，需要以下处理办法
+    true=True
+    false=False
+    none=None
+
     status = return_token(get_token_api, username, password)
     ToKen = status['Access-Token']
-    get_datacenter_info('3766742a-1243-456b-8bdc-e8b5a3b94683', ToKen)
+    print ToKen
+    vcenter_appid = get_vcenter_appid(token=ToKen, vcenter_name='ht')
+    print vcenter_appid
+    dk = get_datacenter_info(ToKen, vcenter_appid)
+    print dk
